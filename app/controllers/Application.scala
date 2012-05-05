d7cc130fd302551988666f957c59ff64978c6183
reski <santiracca3@gmail.com>
2012-05-05 04:40:50 -0500

    battleRoom, first step Websockets

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,41 @@
 package controllers
 
 import play.api._
+import libs.iteratee.{Enumerator, Iteratee}
+import libs.json.JsValue
 import play.api.mvc._
+import models.{Board}
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
+
+  def battleRoom(username: Option[String]) = Action { implicit request =>
+    username.filterNot(_.isEmpty).map { username =>
+
+    if(BoardManager.addPlayer(username)){
+    Ok(views.html.battleRoom(username))
+    }else{
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Battle is full");
+    }
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing(
+        "error" -> "Please choose a valid username."
+      )
+    }
+  }
+  def shoot(username: String) = WebSocket.using[String] { request  =>
+
+    val in =  Iteratee.foreach[String](s => GameController.parse(username,s))
+    //pasar a shoot
+    // Send a single 'Hello!' message and close
+    val out = Enumerator("hello")
+
+   // Board.join(username)
+    (in, out)
   }
   
 }
